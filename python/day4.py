2acf0bcccb97126aaf6258711d10c997db7dbab2
MattBarkway <contact@mattbarkway.dev>
2023-12-04 10:16:49 +0000

    day 4 pt1

---
+++ python/day4.py
@@ -0,0 +1,57 @@
+import collections
+
+from loading import load_input
+
+
+def run():
+    cards = load_input("4.txt")
+    pt_1(cards)
+    pt_2(cards)
+
+
+def pt_1(cards):
+    score = []
+    for card in cards:
+        my_winners = get_winners(card)
+        if my_winners:
+            score.append(2 ** (len(my_winners) - 1))
+
+    return sum(score)
+
+
+def pt_2(cards):
+    copies: dict[int, int] = collections.defaultdict(int)
+
+    for card in cards:
+        card_str, rest = card.split(": ")
+        card_num = int(card_str.split()[1])
+        num_copies = copies[card_num]
+        my_winners = get_winners_pt2(rest)
+        for i in range(len(my_winners) + 1):
+            copies[card_num + i] += 1 + num_copies
+    return sum([v for v in copies.values()])
+
+
+# between 6,499,390 and 38,999,762
+# not 38,999,559
+def get_winners(card: str) -> list[int]:
+    card_num, rest = card.split(": ")
+    winning_str, mine_str = rest.split("|")
+    winning = str_to_nums(winning_str)
+    mine = str_to_nums(mine_str)
+    return [i for i in mine if i in winning]
+
+
+def get_winners_pt2(rest: str) -> list[int]:
+    winning_str, mine_str = rest.split("|")
+    winning = str_to_nums(winning_str)
+    mine = str_to_nums(mine_str)
+    return [i for i in mine if i in winning]
+
+
+def str_to_nums(num_str):
+    return [i.strip() for i in num_str.split()]
+
+
+if __name__ == "__main__":
+    run()
